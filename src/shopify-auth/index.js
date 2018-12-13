0f4e49780415f751a35ce5fb34d2291768388567
Nimrod Izraely <nimrodizr@gmail.com>
2018-12-13 18:47:55 +0200

    fix auth

--- src/shopify-auth/index.js
+++ src/shopify-auth/index.js
@@ -77,6 +77,11 @@ router.get('/callback', (req, res) => {
       .post(accessTokenRequestUrl, { json: accessTokenPayload })
       .then(async (accessTokenResponse) => {
         const accessToken = accessTokenResponse.access_token;
+        const accessTokenCookie = cookie.parse(req.headers.cookie).access_token;
+        if (!accessTokenCookie) {
+          res.cookie('access_token', accessToken);
+        }
+
         if (!(await db.checkIfMerchantExists(shop))) {
           const { _id: merchantId } = await db.addMerchant(shop, accessToken);
           request
@@ -96,25 +101,9 @@ router.get('/callback', (req, res) => {
               console.log('added file');
               res.status(200).send('Installed!');
             });
+        } else {
+          res.status(200).send('Already Installed');
         }
-        const accessTokenCookie = cookie.parse(req.headers.cookie).access_token;
-        if (!accessTokenCookie) {
-          res.cookie('access_token', accessToken);
-        }
-
-        // const shopRequestUrl = `https://${shop}/admin/shop.json`;
-        // const shopRequestHeaders = {
-        //   'X-Shopify-Access-Token': accessToken,
-        // };
-
-        // request
-        //   .get(shopRequestUrl, { headers: shopRequestHeaders })
-        //   .then((shopResponse) => {
-        //     res.status(200).end(shopResponse);
-        //   })
-        //   .catch((error) => {
-        //     res.status(error.statusCode).send(error.error.error_description);
-        //   });
       })
       .catch((error) => {
         res.status(error.statusCode).send(error.error.error_description);
